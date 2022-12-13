8f1071cbacd482e56b94cfa189334130922e5279
omarh119 <ohamad@u.rochester.edu>
2022-12-13 08:56:49 -0500

    Added export of JSON summary report

--- validateBSXMLuse_cunybpl.rb
+++ validateBSXMLuse_cunybpl.rb
@@ -1,3 +1,4 @@
+require 'json'
 require_relative './validateBSXML_cunybpl.rb'
 
 print "For validating single file:\ntype 's'\nFor validating multiple files:\ntype 'm'\n\n"
@@ -19,3 +20,25 @@ print "Enter the version of files you'll validate (e.g. 2.2.0):\n\n"
 vers = gets.strip
 
 BuildingSync::SelectionTool.new(bsxmls,vers)
+
+# Code to create summary report of validation
+# {files with valid schema => [the file's valid use case]}
+schema_valid = {}
+if mode == 'm'
+    Dir.glob(["#{bsxmlFolder}/*.json"]) do |rep|
+        repJSON = JSON.parse(File.read(rep))
+        if repJSON["validation_results"]["schema"]["valid"] == true
+            repJSONsliced = File.basename(rep).gsub(/_bplValidateBSXML_\d\d\d\dT\d\d\d\d\d\d.json/,"")
+            schema_valid.store(repJSONsliced,[])
+            use_cases = repJSON["validation_results"]["use_cases"]
+            use_cases.values.each do |use_case|
+                schema_valid[repJSONsliced] << use_cases.key(use_case) if use_case["valid"] == true
+            end
+        end    
+    end
+end
+puts schema_valid
+puts "#{schema_valid.keys.size} files had valid schema out of #{Dir.glob(["#{bsxmlFolder}/*.json"]).size}"
+File.open("#{bsxmlFolder}/REPORT_bplValidateBSXML_#{File.basename(bsxmlFolder)}.json","w") do |f|
+    f.write(JSON.pretty_generate(schema_valid))
+end
